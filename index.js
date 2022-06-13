4be8dd41cb10f94264e5d0b358a310f9c7df1642
Juliana Oliveira <jsfoliveira89@gmail.com>
2022-06-13 15:36:40 -0300

    Requisito 1

--- index.js
+++ index.js
@@ -12,6 +12,29 @@ app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
+// Requisito 01 - Crie o endpoint GET /talker
+const fs = require('fs').promises;
+
+const fsTalker = ('./talker.json');
+
+async function fetchTalker() {
+  try {
+    const data = await fs.readFile(fsTalker, 'utf8');
+    const result = JSON.parse(data);
+    return result;
+  } catch (error) {
+    console.log(`Erro ${error}`);
+  }
+}
+app.get('/talker', async (_req, res) => {
+  const manageTalkers = await fetchTalker();
+
+  if (manageTalkers) {
+    return res.status(HTTP_OK_STATUS).json(manageTalkers);
+  } 
+  return res.status(HTTP_OK_STATUS).json([]);
+});
+
 app.listen(PORT, () => {
   console.log('Online');
 });
