593c1e4b8cbef48d70d2e3484ba4f2bfe8dc8b0c
Juliana Oliveira <jsfoliveira89@gmail.com>
2022-06-13 16:12:34 -0300

    Requisito 2

--- index.js
+++ index.js
@@ -35,6 +35,20 @@ app.get('/talker', async (_req, res) => {
   return res.status(HTTP_OK_STATUS).json([]);
 });
 
+// 02 - Crie o endpoint GET /talker/:id
+app.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+  const manageTalkers = await fetchTalker();
+  // vou procurar o primeiro elemento do array que contém esse id
+  const findTalkers = manageTalkers.find((element) => element.id === Number(id)); 
+  // se tiver no array o id que estou procurando, então vai retornar o seu conteúdo
+  console.log(findTalkers);
+  if (findTalkers) {
+    return res.json(findTalkers);
+  } 
+    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+});
+
 app.listen(PORT, () => {
   console.log('Online');
 });
